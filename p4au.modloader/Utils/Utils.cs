f00309f77c0bd69a043621f2c143212cab97924c
AnimatedSwine37 <animatedswine37@gmail.com>
2022-05-11 19:24:42 +1000

    Add logging utils

---
+++ p4au.modloader/Utils/Utils.cs
@@ -0,0 +1,34 @@
+﻿using p4au.modloader.Configuration;
+using Reloaded.Mod.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace p4au.modloader.Utilities
+{
+    public static class Utils
+    {
+        private static ILogger logger;
+        private static Config config;
+
+        /// <summary>
+        /// Initiailse the Utils class with necessary stuff
+        /// Only call this once at the start of the program
+        /// </summary>
+        public static void Initialise(ILogger logger, Config config)
+        {
+            Utils.logger = logger;
+            Utils.config = config;
+        }
+
+        public static void LogVerbose(string message)
+        {
+            if(config.Verbose)
+            {
+                logger.WriteLine(message);
+            }
+        }
+    }
+}
